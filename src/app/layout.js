690d968dceb7daabff5e8f9a008d2e580d3aaca0
object505 <pbeluent@gmail.com>
2026-07-20 18:33:33 +0200

    Added ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from 'next-themes'
 import React from 'react'
 import MainLayout from '@/components/shared/MainLayout'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -71,6 +72,7 @@ export default function RootLayout({ children }) {
           </MainLayout>
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-SV868JKQ9S" />
     </html>
   );
 }
